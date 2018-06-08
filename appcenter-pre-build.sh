6ccbe3b2e2df447bae40d7cac189ca24bcf8a144
Stella Widyasari <stella.widyasari@gmail.com>
2018-06-08 14:14:07 +0800

    [Add]Prebuild script

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Example: Change bundle name of an iOS app for non-production
+if [ "$APPCENTER_BRANCH" != "master" ];
+then
+    plutil -replace CFBundleName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/MyApp/Info.plist
+fi
+
+brew update && brew install node
